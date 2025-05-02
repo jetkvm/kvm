58d59eca47508bfda345c98cfa1c859c1c7ea7ec
Daniel Lorch <dlorch@gmail.com>
2025-05-02 22:55:21 +0200

    Add French (France)

--- ui/src/keyboardLayouts.ts
+++ ui/src/keyboardLayouts.ts
@@ -1,4 +1,5 @@
 import { chars as chars_en_US } from "@/keyboardLayouts/en_US"
+import { chars as chars_fr_FR } from "@/keyboardLayouts/fr_FR"
 import { chars as chars_de_DE } from "@/keyboardLayouts/de_DE"
 import { chars as chars_fr_CH } from "@/keyboardLayouts/fr_CH"
 import { chars as chars_de_CH } from "@/keyboardLayouts/de_CH"
@@ -8,6 +9,7 @@ export type KeyCombo = KeyInfo & { deadKey?: boolean, accentKey?: KeyInfo }
 
 export const layouts = {
   "en_US": "English (US)",
+  "fr_FR": "French",
   "de_DE": "German",
   "fr_CH": "Swiss French",
   "de_CH": "Swiss German"
@@ -15,6 +17,7 @@ export const layouts = {
 
 export const chars = {
   "en_US": chars_en_US,
+  "fr_FR": chars_fr_FR,
   "de_DE": chars_de_DE,
   "fr_CH": chars_fr_CH,
   "de_CH": chars_de_CH,
