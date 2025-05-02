33a4f38702904911c72b1c9337ba96fa7e79944c
Daniel Lorch <dlorch@gmail.com>
2025-05-02 21:19:51 +0200

    Add Swiss French

--- ui/src/keyboardLayouts.ts
+++ ui/src/keyboardLayouts.ts
@@ -1,4 +1,5 @@
 import { chars as chars_en_US } from "@/keyboardLayouts/en_US"
+import { chars as chars_fr_CH } from "@/keyboardLayouts/fr_CH"
 import { chars as chars_de_CH } from "@/keyboardLayouts/de_CH"
 
 type KeyInfo = { key: string | number; shift?: boolean, altRight?: boolean }
@@ -6,10 +7,12 @@ export type KeyCombo = KeyInfo & { deadKey?: boolean, accentKey?: KeyInfo }
 
 export const layouts = {
   "en_US": "English (US)",
+  "fr_CH": "Swiss French",
   "de_CH": "Swiss German"
 } as Record<string, string>;
 
 export const chars = {
   "en_US": chars_en_US,
+  "fr_CH": chars_fr_CH,
   "de_CH": chars_de_CH,
 } as Record<string, Record <string, KeyCombo>>
