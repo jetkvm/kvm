af9c8fad09c1e176640d2f87e51cbb4c98d116eb
Daniel Lorch <dlorch@gmail.com>
2025-05-02 23:18:56 +0200

    Add Swedish

--- ui/src/keyboardLayouts.ts
+++ ui/src/keyboardLayouts.ts
@@ -2,6 +2,7 @@ import { chars as chars_en_UK } from "@/keyboardLayouts/en_UK"
 import { chars as chars_en_US } from "@/keyboardLayouts/en_US"
 import { chars as chars_fr_FR } from "@/keyboardLayouts/fr_FR"
 import { chars as chars_de_DE } from "@/keyboardLayouts/de_DE"
+import { chars as chars_sv_SE } from "@/keyboardLayouts/sv_SE"
 import { chars as chars_fr_CH } from "@/keyboardLayouts/fr_CH"
 import { chars as chars_de_CH } from "@/keyboardLayouts/de_CH"
 
@@ -13,6 +14,7 @@ export const layouts = {
   "en_US": "English (US)",
   "fr_FR": "French",
   "de_DE": "German",
+  "sv_SE": "Swedish",
   "fr_CH": "Swiss French",
   "de_CH": "Swiss German"
 } as Record<string, string>;
@@ -22,6 +24,7 @@ export const chars = {
   "en_US": chars_en_US,
   "fr_FR": chars_fr_FR,
   "de_DE": chars_de_DE,
+  "sv_SE": chars_sv_SE,
   "fr_CH": chars_fr_CH,
   "de_CH": chars_de_CH,
 } as Record<string, Record <string, KeyCombo>>
