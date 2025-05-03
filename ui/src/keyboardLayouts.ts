ac42be96e71083eca0c80c920be1ab8685087405
Daniel Lorch <dlorch@gmail.com>
2025-05-03 23:56:56 +0200

    Add Norwegian

--- ui/src/keyboardLayouts.ts
+++ ui/src/keyboardLayouts.ts
@@ -2,6 +2,7 @@ import { chars as chars_en_UK } from "@/keyboardLayouts/en_UK"
 import { chars as chars_en_US } from "@/keyboardLayouts/en_US"
 import { chars as chars_fr_FR } from "@/keyboardLayouts/fr_FR"
 import { chars as chars_de_DE } from "@/keyboardLayouts/de_DE"
+import { chars as chars_nb_NO } from "@/keyboardLayouts/nb_NO"
 import { chars as chars_es_ES } from "@/keyboardLayouts/es_ES"
 import { chars as chars_sv_SE } from "@/keyboardLayouts/sv_SE"
 import { chars as chars_fr_CH } from "@/keyboardLayouts/fr_CH"
@@ -15,6 +16,7 @@ export const layouts = {
   "en_US": "English (US)",
   "fr_FR": "French",
   "de_DE": "German",
+  "nb_NO": "Norwegian",
   "es_ES": "Spanish",
   "sv_SE": "Swedish",
   "fr_CH": "Swiss French",
@@ -26,6 +28,7 @@ export const chars = {
   "en_US": chars_en_US,
   "fr_FR": chars_fr_FR,
   "de_DE": chars_de_DE,
+  "nb_NO": chars_nb_NO,
   "es_ES": chars_es_ES,
   "sv_SE": chars_sv_SE,
   "fr_CH": chars_fr_CH,
