12dbe6fcf06b2bd3690b630035434b6c5d2ca3a4
Siyuan Miao <i@xswan.net>
2025-05-20 14:58:11 +0200

    feat: add -version flag for jetkvm_app

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/jetkvm/kvm"
+	"github.com/prometheus/common/version"
 )
 
+func printVersion() {
+	version.Version = kvm.GetBuiltAppVersion()
+	app_version := version.Print("JetKVM Application")
+	fmt.Println(app_version)
+
+	nativeVersion, err := kvm.GetNativeVersion()
+	if err == nil {
+		fmt.Println("\nJetKVM Native, version", nativeVersion)
+	}
+}
+
 func main() {
+	versionPtr := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *versionPtr {
+		printVersion()
+		return
+	}
+
 	kvm.Main()
 }
