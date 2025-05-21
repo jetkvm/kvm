cf71508bcde3f70f61bf165a9d80486e86d65b86
Adam Shiervani <adam.shiervani@gmail.com>
2025-05-21 12:46:53 +0200

    chore: update ESLint configuration and add Prettier plugin
    - Add eslint-plugin-prettier to integrate Prettier with ESLint.
    - Update linting scripts to include CSS files.

--- ui/eslint.config.cjs
+++ ui/eslint.config.cjs
@@ -1,48 +1,44 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require("eslint/config");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 const globals = require("globals");
 
-const {
-    fixupConfigRules,
-} = require("@eslint/compat");
+const { fixupConfigRules } = require("@eslint/compat");
 
 const tsParser = require("@typescript-eslint/parser");
 const reactRefresh = require("eslint-plugin-react-refresh");
 const js = require("@eslint/js");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
 
-        parserOptions: {
-            project: ["./tsconfig.json", "./tsconfig.node.json"],
-            tsconfigRootDir: __dirname,
-            ecmaFeatures: {
-                jsx: true
-            }
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
-    extends: fixupConfigRules(compat.extends(
+    extends: fixupConfigRules(
+      compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/stylistic",
@@ -51,43 +47,53 @@ module.exports = defineConfig([{
         "plugin:react/jsx-runtime",
         "plugin:import/recommended",
         "prettier",
-    )),
+      ),
+    ),
 
     plugins: {
-        "react-refresh": reactRefresh,
+      "react-refresh": reactRefresh,
     },
 
     rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
 
-        "import/order": ["error", {
-            groups: ["builtin", "external", "internal", "parent", "sibling"],
-            "newlines-between": "always",
-        }],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling"],
+          "newlines-between": "always",
+        },
+      ],
     },
 
     settings: {
-        "react": {
-            "version": "detect"
-        },
-        "import/resolver": {
-            alias: {
-                map: [
-                    ["@components", "./src/components"],
-                    ["@routes", "./src/routes"],
-                    ["@assets", "./src/assets"],
-                    ["@", "./src"],
-                ],
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        alias: {
+          map: [
+            ["@components", "./src/components"],
+            ["@routes", "./src/routes"],
+            ["@assets", "./src/assets"],
+            ["@", "./src"],
+          ],
 
-                extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-            },
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
         },
+      },
     },
-}, globalIgnores([
+  },
+  globalIgnores([
     "**/dist",
     "**/.eslintrc.cjs",
     "**/tailwind.config.js",
     "**/postcss.config.js",
-])]);
+  ]),
+  eslintPluginPrettierRecommended,
+]);
