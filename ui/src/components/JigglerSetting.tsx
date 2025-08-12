0feb5a9c77eacb0804be74a4e369b016d06e31d2
Adam Shiervani <adam.shiervani@gmail.com>
2025-08-12 14:31:37 +0200

    refactor(jiggler): simplify JigglerSetting and integrate with settings page

--- ui/src/components/JigglerSetting.tsx
+++ ui/src/components/JigglerSetting.tsx
@@ -1,13 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 import { Button } from "@components/Button";
-import { SelectMenuBasic } from "@components/SelectMenuBasic";
-
-import { useJsonRpc } from "../hooks/useJsonRpc";
-import notifications from "../notifications";
 
 import { InputFieldWithLabel } from "./InputField";
-
+import ExtLink from "./ExtLink";
 
 export interface JigglerConfig {
   inactivity_limit_seconds: number;
@@ -15,205 +11,84 @@ export interface JigglerConfig {
   schedule_cron_tab: string;
 }
 
-const jigglerCrontabConfigs = [
-  {
-    label: "Every 20 seconds",
-    value: "*/20 * * * * *",
-  },
-  {
-    label: "Every 40 seconds",
-    value: "*/40 * * * * *",
-  },
-  {
-    label: "Every 1 minute",
-    value: "0 * * * * *",
-  },
-  {
-    label: "Every 3 minutes",
-    value: "0 */3 * * * *",
-  },
-];
-
-const jigglerJitterConfigs = [
-  {
-    label: "No Jitter",
-    value: "0",
-  },
-  {
-    label: "10%",
-    value: "20",
-  },
-  {
-    label: "25%",
-    value: "25",
-  },
-  {
-    label: "50%",
-    value: "50",
-  },
-];
-
-const jigglerInactivityConfigs = [
-  {
-    label: "20 Seconds",
-    value: "20",
-  },
-  {
-    label: "40 Seconds",
-    value: "40",
-  },
-  {
-    label: "1 Minute",
-    value: "60",
-  },
-  {
-    label: "3 Minutes",
-    value: "180",
-  },
-];
-
-export function JigglerSetting() {
-  const [send] = useJsonRpc();
-  const [loading, setLoading] = useState(false);
-  const [jitterPercentage, setJitterPercentage] = useState("");
-  const [scheduleCronTab, setScheduleCronTab] = useState("");
-
+export function JigglerSetting({
+  onSave,
+}: {
+  onSave: (jigglerConfig: JigglerConfig) => void;
+}) {
   const [jigglerConfigState, setJigglerConfigState] = useState<JigglerConfig>({
     inactivity_limit_seconds: 20,
-    jitter_percentage:        0,
-    schedule_cron_tab:        "*/20 * * * * *"
+    jitter_percentage: 0,
+    schedule_cron_tab: "*/20 * * * * *",
   });
 
-  const syncJigglerConfig = useCallback(() => {
-    send("getJigglerConfig", {}, resp => {
-      if ("error" in resp) return;
-      const result = resp.result as JigglerConfig;
-      setJigglerConfigState(result);
-
-      const jitterPercentage = jigglerJitterConfigs.map(u => u.value).includes(result.jitter_percentage.toString())
-        ? result.jitter_percentage.toString()
-        : "custom";
-      setJitterPercentage(jitterPercentage)
-
-      const scheduleCronTab = jigglerCrontabConfigs.map(u => u.value).includes(result.schedule_cron_tab)
-        ? result.schedule_cron_tab
-        : "custom";
-      setScheduleCronTab(scheduleCronTab)
-    });
-  }, [send]);
-
-  useEffect(() => {
-    syncJigglerConfig()
-  }, [send, syncJigglerConfig]);
-
-  const handleJigglerInactivityLimitSecondsChange = (value: string) => {
-    setJigglerConfigState({ ...jigglerConfigState, inactivity_limit_seconds: Number(value) });
-  };
-
-  const handleJigglerJitterPercentageChange = (value: string) => {
-    setJigglerConfigState({ ...jigglerConfigState, jitter_percentage: Number(value) });
-  };
-
-  const handleJigglerScheduleCronTabChange = (value: string) => {
-    setJigglerConfigState({ ...jigglerConfigState, schedule_cron_tab: value });
-  };
-
-  const handleJigglerConfigSave = useCallback(
-    (jigglerConfig: JigglerConfig) => {
-      setLoading(true);
-      send("setJigglerConfig", { jigglerConfig }, async resp => {
-        if ("error" in resp) {
-          notifications.error(
-            `Failed to set jiggler config: ${resp.error.data || "Unknown error"}`,
-          );
-          setLoading(false);
-          return;
-        }
-        setLoading(false);
-        notifications.success(
-          `Jiggler Config successfully updated`,
-        );
-        syncJigglerConfig();
-      });
-    },
-    [send, syncJigglerConfig],
-  );
-
   return (
-    <div className="">
-      <div className="grid grid-cols-2 gap-4">
-        <SelectMenuBasic
+    <div className="space-y-2">
+      <div className="grid max-w-sm grid-cols-1 items-end gap-y-2">
+        <InputFieldWithLabel
+          required
           size="SM"
-          label="Schedule"
-          className="max-w-[192px]"
-          value={scheduleCronTab}
-          fullWidth
-          onChange={e => {
-            setScheduleCronTab(e.target.value);
-            if (e.target.value != "custom") {
-              handleJigglerScheduleCronTabChange(e.target.value);
-            }
-          }}
-          options={[...jigglerCrontabConfigs, {value: "custom", label: "Custom"}]}
+          label="Cron Schedule"
+          description={
+            <span>
+              Generate with{" "}
+              <ExtLink className="text-blue-700 underline" href="https://crontab.guru/">
+                crontab.guru
+              </ExtLink>
+            </span>
+          }
+          placeholder="*/20 * * * * *"
+          defaultValue={jigglerConfigState.schedule_cron_tab}
+          onChange={e =>
+            setJigglerConfigState({
+              ...jigglerConfigState,
+              schedule_cron_tab: e.target.value,
+            })
+          }
         />
-        {scheduleCronTab === "custom" && (
-          <InputFieldWithLabel
-            required
-            label="Jiggler Crontab"
-            placeholder="*/20 * * * * *"
-            defaultValue={jigglerConfigState.schedule_cron_tab}
-            onChange={e => handleJigglerScheduleCronTabChange(e.target.value)}
-          />
-        )}
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        <SelectMenuBasic
-          size="SM"
-          label="Jitter Percentage"
-          className="max-w-[192px]"
-          value={jitterPercentage}
-          fullWidth
-          onChange={e => {
-            setJitterPercentage(e.target.value);
-            if (e.target.value != "custom") {
-              handleJigglerJitterPercentageChange(e.target.value)
-            }
-          }}
-          options={[...jigglerJitterConfigs, {value: "custom", label: "Custom"}]}
-        />
-        {jitterPercentage === "custom" && (
-          <InputFieldWithLabel
-            required
-            label="Jitter Percentage"
-            placeholder="25"
-            defaultValue={jigglerConfigState.jitter_percentage}
-            type="number"
-            min="1"
-            max="100"
-            onChange={e => handleJigglerJitterPercentageChange(e.target.value)}
-          />
-        )}
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        <SelectMenuBasic
+
+        <InputFieldWithLabel
           size="SM"
           label="Inactivity Limit Seconds"
-          className="max-w-[192px]"
+          description="Seconds of inactivity before triggering a jiggle again"
           value={jigglerConfigState.inactivity_limit_seconds}
-          fullWidth
-          onChange={e => {
-            handleJigglerInactivityLimitSecondsChange(e.target.value);
-          }}
-          options={[...jigglerInactivityConfigs]}
+          type="number"
+          min="1"
+          max="100"
+          onChange={e =>
+            setJigglerConfigState({
+              ...jigglerConfigState,
+              inactivity_limit_seconds: Number(e.target.value),
+            })
+          }
+        />
+
+        <InputFieldWithLabel
+          required
+          size="SM"
+          label="Random delay"
+          description="To avoid recognizable patterns"
+          placeholder="25"
+          TrailingElm={<span className="px-2 text-xs text-slate-500">%</span>}
+          defaultValue={jigglerConfigState.jitter_percentage}
+          type="number"
+          min="0"
+          max="100"
+          onChange={e =>
+            setJigglerConfigState({
+              ...jigglerConfigState,
+              jitter_percentage: Number(e.target.value),
+            })
+          }
         />
       </div>
+
       <div className="mt-6 flex gap-x-2">
         <Button
-          loading={loading}
           size="SM"
           theme="primary"
-          text="Update Jiggler Config"
-          onClick={() => handleJigglerConfigSave(jigglerConfigState)}
+          text="Save Jiggler Config"
+          onClick={() => onSave(jigglerConfigState)}
         />
       </div>
     </div>
