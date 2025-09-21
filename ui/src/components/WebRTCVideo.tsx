38982ec619b37c7d055c14588fdab938de90d5b2
Alex P <dtk077@gmail.com>
2025-09-21 21:20:05 +0000

    [WIP] Improvements, Bugfixes: Improve audio experience when running in HTTP mode

--- ui/src/components/WebRTCVideo.tsx
+++ ui/src/components/WebRTCVideo.tsx
@@ -43,6 +43,8 @@ interface MicrophoneHookReturn {
   isStarting: boolean;
   isStopping: boolean;
   isToggling: boolean;
+  // HTTP/HTTPS detection
+  isHttpsRequired: boolean;
 }
 
 interface WebRTCVideoProps {
