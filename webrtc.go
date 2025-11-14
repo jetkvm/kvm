68bc4805379967fc339a538d66adf9142df4afad
Siyuan <siyuan@buildjet.com>
2025-11-14 11:58:20 +0000

    fix: remove duplicate triggerOTAStateUpdate call

--- webrtc.go
+++ webrtc.go
@@ -288,7 +288,6 @@ func newSession(config SessionConfig) (*Session, error) {
 			})
 			// Wait for channel to be open before sending initial state
 			d.OnOpen(func() {
-				triggerOTAStateUpdate()
 				triggerVideoStateUpdate()
 				triggerUSBStateUpdate()
 				notifyFailsafeMode(session)
