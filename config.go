8fabba4044af7d710c62100d23e038c4ac117ba5
Alex P <dtk077@gmail.com>
2025-11-18 00:53:10 +0200

    Align inline comments in Config struct

--- config.go
+++ config.go
@@ -109,9 +109,9 @@ type Config struct {
 	VideoQualityFactor   float64              `json:"video_quality_factor"`
 	AudioInputAutoEnable bool                 `json:"audio_input_auto_enable"`
 	AudioOutputEnabled   bool                 `json:"audio_output_enabled"`
-	AudioOutputSource    string               `json:"audio_output_source"`    // "hdmi" or "usb"
-	AudioBitrate         int                  `json:"audio_bitrate"`          // kbps (64-256)
-	AudioComplexity      int                  `json:"audio_complexity"`       // 0-10
+	AudioOutputSource    string               `json:"audio_output_source"` // "hdmi" or "usb"
+	AudioBitrate         int                  `json:"audio_bitrate"`       // kbps (64-256)
+	AudioComplexity      int                  `json:"audio_complexity"`    // 0-10
 	AudioDTXEnabled      bool                 `json:"audio_dtx_enabled"`
 	AudioFECEnabled      bool                 `json:"audio_fec_enabled"`
 	AudioBufferPeriods   int                  `json:"audio_buffer_periods"`   // 2-24
