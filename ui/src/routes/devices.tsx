89c9be77114c9bde8901e83ac8c34dc07b30b36e
Siyuan Miao <i@xswan.net>
2025-11-24 13:17:23 +0100

    backport: versioning cloud app (0.4.4)

--- ui/src/routes/devices.tsx
+++ ui/src/routes/devices.tsx
@@ -12,7 +12,13 @@ import { checkAuth } from "@/main";
 import { CLOUD_API } from "@/ui.config";
 
 interface LoaderData {
-  devices: { id: string; name: string; online: boolean; lastSeen: string }[];
+  devices: {
+    id: string;
+    name: string;
+    online: boolean;
+    lastSeen: string;
+    version: string;
+  }[];
   user: User;
 }
 
@@ -89,6 +95,7 @@ export default function DevicesRoute() {
                         title={x.name ?? x.id}
                         lastSeen={x.lastSeen ? new Date(x.lastSeen) : null}
                         online={x.online}
+                        appVersion={x.version}
                       />
                     );
                   })}
